664ea23140083af2262da7b11781eeb7e0bf5dc7
jeffreyd3308 <jeffreyd3308@nycstudents.net>
2024-12-17 17:27:05 +0000

    day3

--- src/main/java/com/example/project/Day3.java
+++ src/main/java/com/example/project/Day3.java
@@ -4,14 +4,21 @@ public class Day3 {
       public static String[][] generateSnowflake(int size) { //you will be tested on this method
         String[][] grid = new String[size][size];
 
-    
+        for (int i = 0; i < size; i++) {
+          for (int j = 0; j < size; j++) {
+              if (i == j || i + j == size - 1 || i == (size / 2) || j == (size / 2)) {
+                grid[i][j] = "*";
+              } else {
+                grid[i][j] = " ";
+              }
+          }
+        }
 
         return grid;
     }
 
     // Prints the snowflake  will be useful if tests fail (you will not be tested on this method)
-    public static void printSnowflake(String[][] snowflake) {
-        
+    public static void printSnowflake() {
     }
 
     // Test for the snowflake generation
--- src/main/java/com/example/project/Day4.java
+++ src/main/java/com/example/project/Day4.java
@@ -7,24 +7,49 @@ public class Day4 {
 
     public static class Reindeer { // you must have at least a name, distance travelled, speed, run duration, and rest duration attribute
 
+        private String name;
+        private int speed;
+        private int runDuration;
+        private int restDuration;
+        private int distance;
+        private static boolean inRest = false;
+        private static int time = 0;
 
         // Constructor 
         public Reindeer(String name, int speed, int runDuration, int restDuration) {
+            this.name = name;
+            this.speed = speed;
+            this.runDuration = runDuration;
+            this.restDuration = restDuration;
+            distance = 0;
+        }
+
+        public String getName(){
+            return name;
         }
 
         public int getDistanceTraveled(){ // this method is required
-            return 0;
+            return distance;
         }
         
 
         public void simulateSecond(){ //this method is required but not tested
+            time++;
+            if (!inRest) {
+                distance += speed;
+            }
+            if (time % runDuration + restDuration == 0) {
+                inRest = false;
+            } else if (time % runDuration + restDuration >= runDuration) {
+                inRest = true;
+            }
             
         }
 
     }
 
     public static String simulateRace(int time, Reindeer[] reindeers){ //you will be tested on this method
-        return "name of winner";
+        return "";
     }
 
     
